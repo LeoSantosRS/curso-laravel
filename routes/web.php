cefaa8c19b20ff822686c6731af7b83a4111abd2
LeoSantosRS <bigsoftsbrasil@gmail.com>
2020-05-18 17:50:32 -0300

    primeiro controller

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,7 @@ Route::middleware(['auth'])->group(function (){
         Route::get('/produtos', function () {
             return 'produtos Admin';
         });
+        Route::get('/','Admin/TesteController@teste');
     });   
 });
 
